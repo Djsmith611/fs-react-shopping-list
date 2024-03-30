b9ba66f53590c9992e96a7e851c93de8cf2601a5
David Smith <djsmith611@me.com>
2024-03-30 08:55:51 -0700

    reset bought items set up in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './Header.css';
 
-function Header({ setIsEditMode }) {
+function Header({ setIsEditMode ,isEditMode, getItems}) {
+
+    const resetBoughtStatus = () => {
+        axios
+            .put('/api/groceries')
+            .then((res) => {
+                getItems();
+            })
+            .catch((err) => {
+                console.error(err);
+                alert('Something went worng!');
+            });
+    } 
     
     return (
         <header className="banner-header">
             <h1>My Shopping List</h1>
             <button onClick={setIsEditMode}>Edit</button>
+            {isEditMode && (
+                <button onClick={resetBoughtStatus}>Reset Bought Items</button>
+            )}
+            
         </header>
     );
 }
