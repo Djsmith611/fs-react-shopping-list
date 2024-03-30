aae4970951daf9ad37c45b51a551442d977f8ca4
David Smith <djsmith611@me.com>
2024-03-30 08:25:19 -0700

    added code for edit mode functionality in List.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,65 +1,91 @@
-import {useState, useEffect} from 'react';
-import './List.css';
+import { useState, useEffect } from "react";
+import "./List.css";
 
-function List() {
-    // List array
-    const [itemList, setItemList] = useState([]);
-    // GET
-    const getItems = () => {
-        axios.get('/api/groceries').then((res) => {
-            setItemList(res.data)
-        }).catch((err) => {
-            console.error(err);
-            alert('Something went wrong!');
-        });
-    }
-    // DELETE
-    const deleteItem = (id) => {
-        axios.delete(`/api/groceries/${id}`).then((res) => {
-            getItems();
-        }).catch((err) => {
-            console.error(err);
-            alert('Something went wrong!');
-        });
-    } 
-
-    // PUT
-    const buyItem = (id) => {
-        axios.put(`/api/groceries/${id}`).then((res) => {
-            getItems();
-        }).catch((err) => {
-            console.error(err);
-            alert('Something went wrong!');
-        });
-    }
-
-    // ToDo: hidden buttons
-
-    useEffect(() => {
+function List({ isEditMode }) {
+  // List array
+  const [itemList, setItemList] = useState([]);
+  // GET
+  const getItems = () => {
+    axios
+      .get("/api/groceries")
+      .then((res) => {
+        setItemList(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Something went wrong!");
+      });
+  };
+  // DELETE
+  const deleteItem = (id) => {
+    axios
+      .delete(`/api/groceries/${id}`)
+      .then((res) => {
         getItems();
-    }, []);
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Something went wrong!");
+      });
+  };
 
-    return (
-        <div>
-            {itemList.map((item) => (
-                <div key={item.id}>
-                    <h3>{item.name}</h3>
-                    {item.displayImage ? (
-                        <img src={item.displayImage} alt={item.name} />
-                    ) : (
-                        <div className='no-image'>
-                            <span>No Image</span>
-                        </div>
-                    )}
-                    <h4>{item.quantity}{item.unit}</h4>
-                    <div>
-                        <button onClick={() => {buyItem(item.id)}}>Buy</button>
-                        <button onClick={() => {deleteItem(item.id)}}>Remove</button>
-                    </div>
-                </div>
-            ))}
-        </div> 
-    )
+  // PUT
+  const buyItem = (id) => {
+    axios
+      .put(`/api/groceries/${id}`)
+      .then((res) => {
+        getItems();
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Something went wrong!");
+      });
+  };
+
+  // ToDo: hidden buttons
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  return (
+    <div>
+      {itemList.map((item) => (
+        <div key={item.id}>
+          <h3>{item.name}</h3>
+          {item.displayImage ? (
+            <img src={item.displayImage} alt={item.name} />
+          ) : (
+            <div className="no-image">
+              <span>No Image</span>
+            </div>
+          )}
+          <h4>
+            {item.quantity}
+            {item.unit}
+          </h4>
+          {isEditMode && (
+            <div>
+              <button
+                onClick={() => {
+                  buyItem(item.id);
+                }}
+              >
+                Buy
+              </button>
+              <button
+                onClick={() => {
+                  deleteItem(item.id);
+                }}
+              >
+                Remove
+              </button>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default List;
